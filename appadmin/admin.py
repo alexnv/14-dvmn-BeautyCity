9bda878b1a8edb5f5072db916b53cf4c3dda7c96
Непряхин Алексей <aleksey.nepryahin@outlook.com>
2023-05-29 19:37:06 +0300

    add feedback to admin

--- appadmin/admin.py
+++ appadmin/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from appadmin.models import BeautySaloon, Serivice, Employee, Schedule, Customer
+from appadmin.models import BeautySaloon, Serivice, Employee, Schedule, Customer, Feedback
 
 # Register your models here.
 admin.site.register(BeautySaloon)
@@ -8,3 +8,4 @@ admin.site.register(Employee)
 admin.site.register(Serivice)
 admin.site.register(Customer)
 admin.site.register(Schedule)
+admin.site.register(Feedback)
